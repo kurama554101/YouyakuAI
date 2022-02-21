6acc1e4faea255caf577320a3c9c1bc02f0472b6
kurama554101 <shotasakamoto554101@gmail.com>
2022-02-21 23:23:34 +0900

    tmp commit

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
 provider "google" {
   project = "youyaku-ai"
   region  = "us-central1"
